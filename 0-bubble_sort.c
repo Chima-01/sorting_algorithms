07c9d04486ed01375e1c47526ea99acab0a721c8
Chima-01 <divinechima76@gmail.com>
2023-07-25 11:13:53 -0700

    fixed bug

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,7 +11,7 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j, flag;
 
-	if (array == NULL || size == 0)
+	if (array == NULL || size <= 0)
 		return;
 
 	for (i = 0; i < size - 1; i++)
